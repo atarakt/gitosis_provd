873314a2249f5290c9b6c69748297cb59ed5b4e7
Nicolas HICHER (atarakt) <nhicher@proformatique.com>
2011-12-02 17:51:15 -0500

    initial commit

---
+++ test_gitosis_prov.py
@@ -0,0 +1,10 @@
+import unittest
+from gitosis_prov import GenerateGitosisConfigFile
+
+Class TestGenerateGitosisConfigFile(unittest.TestCase):
+    def test_open_config_file(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
+
