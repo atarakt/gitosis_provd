1ad5c6d8b88449c1fadbd82ab6e8b986d046b5d8
atarakt <atarakt@gmail.com>
2011-12-03 17:32:40 -0500

    [evol] add create group and repo functions

--- gitosis_prov.py
+++ gitosis_prov.py
@@ -2,27 +2,44 @@ from configobj import ConfigObj
 
 class GenerateGitosisConfigFile:
     def __init__(self):
-        self.repo            = 'gitosis'
-        self.web_default     = 'no'
-        self.daemon_default  = 'no'
         config_file          = 'repositories.conf'
         self.config          = ConfigObj(config_file)
 
     def _get_officials_repo(self):
-        return self.config['repositories']['officials']
+        return self.config['repositories']['official']
 
     def _get_privates_repo(self):
-        return self.config['repositories']['privates']
+        return self.config['repositories']['private']
 
-    def _get_team(self):
-        return self.config['xivo_team']['xivo']
+    def _get_group(self, group):
+        return [group, self.config['groups'][group]]
 
     def _get_extra_data(self):
         return self.config['extra_data']
     
     def _create_skel(self):
-        repo   = "[%s]" % self.repo
-        web    = "gitweb = %s"    % self.web_default
-        daemon = "daemon = %s" % self.daemon_default
-        return ("%s\n%s\n%s\n" % (repo, web, daemon))
+        return ('[gitosis]\n')
 
+    def _create_group(self, group):
+        group_name, users = self._get_group(group)
+        description = '[group %s]\n' % group_name
+        writable = 'writable = %s\n' % group_name
+        members = 'members = %s\n'  % ', '.join(users)
+        return ('%s%s%s\n' % (description, writable, members))
+
+    def _create_repository(self, repository):
+        '''If repository is official, it will be browsable by anyone'''
+        branch, repo = repository.split('/')
+        title = '[repo %s]\n' % repository 
+        owner = 'owner = XiVO Dev Team\n'
+        desc = 'description = %s\n' % self.config['repositories'][branch][repo]
+        gitweb = 'gitweb = yes\n' if branch == 'official' else ''
+        daemon = 'daemon = yes\n' if branch == 'official' else ''
+        return title + owner + desc + gitweb + daemon
+    
+    def _create_config_file(self):
+        config_file = open('gitosis.conf', 'w')
+        config_file.write(self._create_skel())
+        config_file.write(self._create_group('gitosis-admin'))
+        config_file.close()
+        
--- test_gitosis_prov.py
+++ test_gitosis_prov.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 from gitosis_prov import GenerateGitosisConfigFile
 
 class TestGenerateGitosisConfigFile(unittest.TestCase):
@@ -7,24 +8,41 @@ class TestGenerateGitosisConfigFile(unittest.TestCase):
         self.config = GenerateGitosisConfigFile()
 
     def test_parse_officials_repo(self):
-        expected = ['xivo-skaro', 'xivo-gallifrey']
+        expected = {'xivo-skaro': 'XiVO Skaro', 'xivo-gallifrey': 'XiVO Gallifrey'}
         self.assertEqual(self.config._get_officials_repo(), expected)
 
     def test_parse_privates_repo(self):
-        expected = ['hard-img', 'hard-code']
+        expected = {'hard-img': 'Hardware img', 'hard-code': 'Hardware code'}
         self.assertEqual(self.config._get_privates_repo(), expected)
 
-    def test_parse_dev(self):
-        expected = ['dev1', 'dev2']
-        self.assertEqual(self.config._get_team(), expected)
+    def test_get_group(self):
+        expected = ['gitosis-admin', ['admin1', 'admin2']]
+        self.assertEqual(self.config._get_group('gitosis-admin'), expected)
 
     def test_parse_extra(self):
         expected = {'hard-img': 'extra_dev1', 'xivo-skaro': 'extra_dev2'}
         self.assertEqual(self.config._get_extra_data(), expected)
 
     def test_create_skel(self):
-        expected = "[gitosis]\ngitweb = no\ndaemon = no\n"
+        expected = '[gitosis]\n'
         self.assertEqual(self.config._create_skel(), expected)
+
+    def test_create_admin_group(self):
+        expected = '[group gitosis-admin]\nwritable = gitosis-admin\nmembers = admin1, admin2\n\n'
+        self.assertEqual(self.config._create_group('gitosis-admin'), expected)
+
+    def test_create_official_repository(self):
+        expected = '[repo official/xivo-skaro]\nowner = XiVO Dev Team\ndescription = XiVO Skaro\ngitweb = yes\ndaemon = yes\n'
+        self.assertEqual(self.config._create_repository('official/xivo-skaro'), expected)
+
+    def test_create_private_repository(self):
+        expected = '[repo private/hard-img]\nowner = XiVO Dev Team\ndescription = Hardware img\n'
+        self.assertEqual(self.config._create_repository('private/hard-img'), expected)
+
+    def test_write_config_file(self):
+        os.remove('gitosis.conf')
+        self.config._create_config_file()
+        self.assertTrue(os.path.isfile('gitosis.conf'))
 
 if __name__ == '__main__':
     unittest.main()
