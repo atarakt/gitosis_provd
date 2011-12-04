bbe77fe71e07f454af3270566c6c5b8e200420a9
atarakt <atarakt@gmail.com>
2011-12-03 23:16:46 -0500

    [evol] manage extra user, refactoring

--- gitosis_prov.py
+++ gitosis_prov.py
@@ -5,40 +5,60 @@ class GenerateGitosisConfigFile:
         config_file          = 'repositories.conf'
         self.config          = ConfigObj(config_file)
 
-    def _get_officials_repo(self):
-        return self.config['repositories']['official']
+    def _get_users(self, group):
+        path = group.split('/')[0]
+        if path == 'official' or path == 'private':
+            users = ['@xivo-core-team']
+            if self._get_extra_options().has_key(group):
+                users.append(self._get_extra_options().get(group))
+        else:
+            users = self.config['groups'][group]
+        return ' '.join(users)
 
-    def _get_privates_repo(self):
-        return self.config['repositories']['private']
-
-    def _get_group(self, group):
-        return [group, self.config['groups'][group]]
-
-    def _get_extra_data(self):
-        return self.config['extra_data']
+    def _get_extra_options(self):
+        return self.config['extra_options']
     
+    def _get_repositories_names(self):
+        result = {}
+        for item in self.config['repositories'].iteritems():
+            branch, repositories = item
+            for data in repositories.iteritems():
+                name, desc = data
+                name = "%s/%s" % (branch, name)
+                result[name] = desc
+        return result
+
     def _create_skel(self):
         return ('[gitosis]\n')
 
+    def _get_repositories(self, group_name):
+        repositories = None if group_name == 'xivo-core-team' else group_name
+        return repositories
+
     def _create_group(self, group):
-        group_name, users = self._get_group(group)
-        description = '[group %s]\n' % group_name
-        writable = 'writable = %s\n' % group_name
-        members = 'members = %s\n'  % ', '.join(users)
-        return ('%s%s%s\n' % (description, writable, members))
+        users = self._get_users(group)
+        title    = '[group %s]\n' % group
+        data     = self._get_repositories(group)
+        writable = 'writable = %s\n' % data if data is not None else ''
+        members  = 'members = %s\n'  % self._get_users(group)
+        return title + writable + members + '\n'
 
     def _create_repository(self, repository):
-        '''If repository is official, it will be browsable by anyone'''
         branch, repo = repository.split('/')
-        title = '[repo %s]\n' % repository
-        owner = 'owner = XiVO Dev Team\n'
-        desc = 'description = %s\n' % self.config['repositories'][branch][repo]
+        title  = '[repo %s]\n' % repository
+        owner  = 'owner = XiVO Dev Team\n'
+        desc   = 'description = %s\n' % self.config['repositories'][branch][repo]
         gitweb = 'gitweb = yes\n' if branch == 'official' else ''
         daemon = 'daemon = yes\n' if branch == 'official' else ''
-        return title + owner + desc + gitweb + daemon
+        return title + owner + desc + gitweb + daemon + '\n'
 
     def _create_config_file(self):
         config_file = open('gitosis.conf', 'w')
         config_file.write(self._create_skel())
         config_file.write(self._create_group('gitosis-admin'))
+        config_file.write(self._create_group('xivo-core-team'))
+        repositories = sorted(self._get_repositories_names().keys())
+        for repository in repositories:
+            config_file.write(self._create_group(repository))
+            config_file.write(self._create_repository(repository))
         config_file.close()
--- test_gitosis_prov.py
+++ test_gitosis_prov.py
@@ -7,37 +7,64 @@ class TestGenerateGitosisConfigFile(unittest.TestCase):
         config = GenerateGitosisConfigFile()
         self.config = GenerateGitosisConfigFile()
 
-    def test_parse_officials_repo(self):
-        expected = {'xivo-skaro': 'XiVO Skaro', 'xivo-gallifrey': 'XiVO Gallifrey'}
-        self.assertEqual(self.config._get_officials_repo(), expected)
-
-    def test_parse_privates_repo(self):
-        expected = {'hard-img': 'Hardware img', 'hard-code': 'Hardware code'}
-        self.assertEqual(self.config._get_privates_repo(), expected)
-
-    def test_get_group(self):
-        expected = ['gitosis-admin', ['admin1', 'admin2']]
-        self.assertEqual(self.config._get_group('gitosis-admin'), expected)
-
     def test_parse_extra(self):
-        expected = {'hard-img': 'extra_dev1', 'xivo-skaro': 'extra_dev2'}
-        self.assertEqual(self.config._get_extra_data(), expected)
+        expected = {'official/xivo-skaro': 'extra_dev1'}
+        self.assertEqual(self.config._get_extra_options(), expected)
+
+    def test_repositories_names(self):
+        expected = {}
+        expected['official/xivo-skaro']     = 'XiVO Skaro'
+        expected['official/xivo-gallifrey'] = 'XiVO Gallifrey'
+        expected['private/hard-img']        = 'Hardware img'
+        expected['private/hard-code']       = 'Hardware code'
+        result = self.config._get_repositories_names()
+        self.assertEqual(result, expected)
 
     def test_create_skel(self):
         expected = '[gitosis]\n'
         self.assertEqual(self.config._create_skel(), expected)
 
     def test_create_admin_group(self):
-        expected = '[group gitosis-admin]\nwritable = gitosis-admin\nmembers = admin1, admin2\n\n'
-        self.assertEqual(self.config._create_group('gitosis-admin'), expected)
+        title    = '[group gitosis-admin]\n'
+        writable = 'writable = gitosis-admin\n'
+        members  = 'members = admin1 admin2\n'
+        expected = title + writable + members + '\n'
+        result   = self.config._create_group('gitosis-admin')
+        self.assertEqual(result, expected)
+
+    def test_create_core_team_group(self):
+        title    = '[group xivo-core-team]\n'
+        members  = 'members = dev1 dev2\n' 
+        expected = title + members + '\n'
+        result   = self.config._create_group('xivo-core-team')
+        self.assertEqual(result, expected)
+
+    def test_create_group_for_repository(self):
+        title    = '[group official/xivo-skaro]\n'
+        writable = 'writable = official/xivo-skaro\n'
+        members  = 'members = @xivo-core-team extra_dev1\n' 
+        expected = title + writable + members + '\n'
+        result   = self.config._create_group('official/xivo-skaro')
+        self.assertEqual(result, expected)
+
 
     def test_create_official_repository(self):
-        expected = '[repo official/xivo-skaro]\nowner = XiVO Dev Team\ndescription = XiVO Skaro\ngitweb = yes\ndaemon = yes\n'
-        self.assertEqual(self.config._create_repository('official/xivo-skaro'), expected)
+        title    = '[repo official/xivo-skaro]\n'
+        owner    = 'owner = XiVO Dev Team\n'
+        desc     = 'description = XiVO Skaro\n'
+        gitweb   = 'gitweb = yes\n'
+        daemon   = 'daemon = yes\n'
+        expected = title + owner + desc + gitweb + daemon + '\n'
+        result   = self.config._create_repository('official/xivo-skaro')
+        self.assertEqual(result, expected)
 
     def test_create_private_repository(self):
-        expected = '[repo private/hard-img]\nowner = XiVO Dev Team\ndescription = Hardware img\n'
-        self.assertEqual(self.config._create_repository('private/hard-img'), expected)
+        title    = '[repo private/hard-img]\n'
+        owner    = 'owner = XiVO Dev Team\n'
+        desc     = 'description = Hardware img\n'
+        expected = title + owner + desc + '\n'
+        result   = self.config._create_repository('private/hard-img')
+        self.assertEqual(result, expected)
 
     def test_write_config_file(self):
         os.remove('gitosis.conf')
@@ -46,4 +73,3 @@ class TestGenerateGitosisConfigFile(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
